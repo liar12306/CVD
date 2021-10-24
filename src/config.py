e9bda1caf31fbaf42fcb3e24503d2816d3ca2b7d
liar <liar1314@126.com>
2021-10-24 21:28:08 +0800

    "2021/10/24 change data_path"

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import os
-PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__))).replace('\\','/')
-with open(PROJECT_ROOT+"/path.txt","r") as f:
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__))).replace('\\', '/')
+with open(PROJECT_ROOT + "/path.txt", "r") as f:
     video_path = f.readline().strip('\n')
 DATA_PATH = "/data/train/"
 ROI_NUM = 5
@@ -8,5 +9,5 @@ ROI_COMBINATION_NUM = 31
 MAP_CHANEL_NUM = 6
 CLIP_LENGTH = 300
 
-video_data_list_file = video_path+"test.txt"
+video_data_list_file = video_path + "stable_data_list.txt"
 train_data_paths = "/data/train.txt"
