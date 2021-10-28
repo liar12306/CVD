984267fa14d80ee03c1c7c58e1604d5ec7c1c7eb
liar <liar1314@126.com>
2021-10-28 22:28:39 +0800

    "2021/10/28 3"

--- src/config.py
+++ src/config.py
@@ -3,11 +3,11 @@ import os
 PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__))).replace('\\', '/')
 with open(PROJECT_ROOT + "/path.txt", "r") as f:
     video_path = f.readline().strip('\n')
-DATA_PATH = "/data/train/"
+DATA_PATH = "/data/train_data/"
 ROI_NUM = 6
 ROI_COMBINATION_NUM = 63
 MAP_CHANEL_NUM = 6
 CLIP_LENGTH = 300
 # tets
-video_data_list_file = video_path + "data_list/test.txt"
+video_data_list_file = video_path + "data_list/stable_data_list.txt"
 train_data_paths = "/data/train.txt"
